e388fb67d1add501285c4f48f70979cb8e3bdce1
Bryan Murasira <bryanmurasira@gmail.com>
2024-01-04 20:49:23 +0200

    Implemented create method in drizzle repository

--- src/Repository/DrizzleRepository.ts
+++ src/Repository/DrizzleRepository.ts
@@ -1,13 +1,13 @@
 import { NeonHttpDatabase } from "drizzle-orm/neon-http";
 import { BlogClass } from "../Domain";
 import { IBlogRepository } from "../Interfaces/IBlogRepository";
-import { blog } from "../Utils/db/schema";
+import { blog as blogSchema }  from "../Utils/db/schema";
 
 export default class DrizzleRepository implements IBlogRepository {
   constructor(private readonly db: NeonHttpDatabase) {}
 
   async readAll(): Promise<BlogClass[]> {
-    const blogs = await this.db.select().from(blog).execute();
+    const blogs = await this.db.select().from(blogSchema).execute();
 
     if (!blogs) {
       return [];
@@ -24,8 +24,19 @@ export default class DrizzleRepository implements IBlogRepository {
       });
     });
   }
-  async create(blog: BlogClass): Promise<{ id: string }> {
-    throw new Error("Method not implemented.");
+  async create(blog: BlogClass): Promise<{ id: unknown }> {
+    
+    const id = await this.db.insert(blogSchema).values({
+      id: blog.id,
+      title: blog.title,
+      content: blog.content,
+      createdAt: blog.createdAt,
+      coverImg: blog.coverImg,
+      author: blog.author, // Add the 'author' property
+    }).returning({id: blogSchema.id}).execute();
+
+    return { id: id[0].id };
+
   }
   async findWithId(id: string): Promise<BlogClass | null> {
     throw new Error("Method not implemented.");
